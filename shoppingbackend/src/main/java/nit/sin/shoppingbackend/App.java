e15fd40314766db07f73266c6f65af1fafd3e2b9
Nishant Singh <nishant.singh@somaiya.edu>
2018-08-31 09:31:31 +0530

    doing project again

---
+++ shoppingbackend/src/main/java/nit/sin/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package nit.sin.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
